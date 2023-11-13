f734365479890c1e2d5b12e704e6c388db9ed4ed
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-13 12:04:59 +0330

    - Added HomeNavigation.kt

---
+++ shared/src/commonMain/kotlin/presentation/navigation/HomeNavigation.kt
@@ -0,0 +1,13 @@
+package presentation.navigation
+
+sealed class HomeNavigation(
+    val route: String,
+    val objectName: String = "",
+    val objectPath: String = ""
+) {
+    object Home : AppNavigation(route = "Home")
+
+    object Detail : AppNavigation(route = "Detail")
+
+}
+
