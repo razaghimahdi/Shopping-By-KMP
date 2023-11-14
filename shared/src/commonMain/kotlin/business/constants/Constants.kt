896798d6175cd032f8abd6cb90657db241cb3059
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-14 20:05:07 +0330

    - Added Constants.kt, DataStoreKeys.kt, KtorConstants.kt

---
+++ shared/src/commonMain/kotlin/business/constants/Constants.kt
@@ -0,0 +1,6 @@
+package business.constants
+
+const val AUTHORIZATION_BEARER_TOKEN = "Bearer "
+const val REFRESH_SCREEN = "refresh "
+
+const val NO_NEED_TO_RESEND = "NO_NEED_TO_RESEND"
---
+++ shared/src/commonMain/kotlin/business/constants/DataStoreKeys.kt
@@ -0,0 +1,5 @@
+package business.constants
+
+object DataStoreKeys {
+    val TOKEN = "com.razzaghi.shoppingbykmp.TOKEN"
+}
