cd6040ea4d48bfc20da81e993a414d9dae314f03
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-18 17:35:25 +0330

    - Updated KtorHttpClient.kt

--- shared/src/commonMain/kotlin/business/core/KtorHttpClient.kt
+++ shared/src/commonMain/kotlin/business/core/KtorHttpClient.kt
@@ -3,7 +3,13 @@ package business.core
 import io.ktor.client.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.request.*
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
@@ -19,19 +25,59 @@ object KtorHttpClient {
     }
 
 
-    fun httpClient() = HttpClient {
-        expectSuccess = false
-        install(HttpTimeout) {
-            val timeout = 30000L
-            connectTimeoutMillis = timeout
-            requestTimeoutMillis = timeout
-            socketTimeoutMillis = timeout
+    fun httpClient() = HttpClient() {
+            expectSuccess = false
+            install(HttpTimeout) {
+                val timeout = 60000L
+                connectTimeoutMillis = timeout
+                requestTimeoutMillis = timeout
+                socketTimeoutMillis = timeout
+            }
+
+
+
+        install(ResponseObserver) {
+            onResponse { response ->
+                println("AppDebug HTTP ResponseObserver status: ${response.status.value}")
+            }
         }
-        install(ContentNegotiation) {
-            json( )
+        HttpResponseValidator {
+            validateResponse { response: HttpResponse ->
+                val statusCode = response.status.value
+
+                /*
+                                    when (statusCode) {
+                                        in 300..399 -> throw RedirectResponseException(response)
+                                        in 400..499 -> throw ClientRequestException(response)
+                                        in 500..599 -> throw ServerResponseException(response)
+                                    }
+
+                                    if (statusCode >= 600) {
+                                        throw ResponseException(response)
+                                    }
+                                }
+
+                                handleResponseException { cause: Throwable ->
+                                    throw cause
+                                }*/
+            }
+        }
+
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    explicitNulls = false
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                    prettyPrint = true
+                    encodeDefaults = true
+                    classDiscriminator = "#class"
+                })
+            }
 
         }
-
-    }
 
 }
