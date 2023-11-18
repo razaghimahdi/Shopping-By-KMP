cb9599e19e2464decd293b00c75bdf4b949658e2
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-18 17:33:32 +0330

    - Updated main.ios.kt

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,4 +1,7 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import common.Context
+import platform.darwin.NSObject
 import presentation.App
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App(Context()) }
+
