18748c55538322724c7909a74bc76f1dea95c19f
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-18 17:42:02 +0330

    - Updated MainNav - Updated App

--- shared/src/commonMain/kotlin/presentation/App.kt
+++ shared/src/commonMain/kotlin/presentation/App.kt
@@ -1,61 +1,52 @@
 package presentation
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import common.Context
+import di.appModule
 import moe.tlaster.precompose.PreComposeApp
 import moe.tlaster.precompose.navigation.NavHost
-import moe.tlaster.precompose.navigation.path
 import moe.tlaster.precompose.navigation.rememberNavigator
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import org.koin.compose.KoinApplication
 import presentation.navigation.AppNavigation
 import presentation.theme.AppTheme
 import presentation.ui.main.MainNav
 import presentation.ui.splash.SplashNav
-import presentation.ui.splash.SplashScreen
 
-@OptIn(ExperimentalCoroutinesApi::class, ExperimentalResourceApi::class)
 @Composable
-internal fun App() {
-    PreComposeApp {
-        val navigator = rememberNavigator()
+internal fun App(context: Context) {
 
+    KoinApplication(application = {
+        modules(appModule(context))
+    }) {
+        PreComposeApp {
 
-        AppTheme {
-            Box(modifier = Modifier.fillMaxSize()) {
-                NavHost(
-                    navigator = navigator,
-                    initialRoute = AppNavigation.Splash.route,
-                ) {
-                    scene(route = AppNavigation.Splash.route) {
-                        SplashNav(navigateToMain = {
-                            navigator.popBackStack()
-                            navigator.navigate(AppNavigation.Main.route)
-                        })
-                    }
-                    scene(route = AppNavigation.Main.route) {
-                        MainNav()
+            AppTheme {
+                val navigator = rememberNavigator()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    NavHost(
+                        navigator = navigator,
+                        initialRoute = AppNavigation.Splash.route,
+                    ) {
+                        scene(route = AppNavigation.Splash.route) {
+                            SplashNav(navigateToMain = {
+                                navigator.popBackStack()
+                                navigator.navigate(AppNavigation.Main.route)
+                            })
+                        }
+                        scene(route = AppNavigation.Main.route) {
+                            MainNav()
+                        }
                     }
                 }
+
             }
         }
     }
 }
+
+
 
 
--- shared/src/commonMain/kotlin/presentation/ui/main/MainNav.kt
+++ shared/src/commonMain/kotlin/presentation/ui/main/MainNav.kt
@@ -2,9 +2,14 @@ package presentation.ui.main
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -31,6 +36,7 @@ import presentation.ui.main.home.HomeScreen
 import presentation.ui.main.profile.ProfileScreen
 import presentation.ui.main.wishlist.WishlistScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainNav() {
 
@@ -38,25 +44,25 @@ fun MainNav() {
 
     Scaffold(bottomBar = {
         BottomNavigationUI(navigator)
-    }) {innerPadding ->
+    }) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
-        NavHost(
-            navigator = navigator,
-            initialRoute = MainNavigation.Home.route,
-        ) {
-            scene(route = MainNavigation.Home.route) {
-                HomeNav()
+            NavHost(
+                navigator = navigator,
+                initialRoute = MainNavigation.Home.route,
+            ) {
+                scene(route = MainNavigation.Home.route) {
+                    HomeNav()
+                }
+                scene(route = MainNavigation.Wishlist.route) {
+                    WishlistScreen()
+                }
+                scene(route = MainNavigation.Cart.route) {
+                    CartScreen()
+                }
+                scene(route = MainNavigation.Profile.route) {
+                    ProfileScreen()
+                }
             }
-            scene(route = MainNavigation.Wishlist.route) {
-                WishlistScreen()
-            }
-            scene(route = MainNavigation.Cart.route) {
-                CartScreen()
-            }
-            scene(route = MainNavigation.Profile.route) {
-                ProfileScreen()
-            }
-        }
         }
 
     }
@@ -65,35 +71,45 @@ fun MainNav() {
 
 @Composable
 fun BottomNavigationUI(navigator: Navigator) {
-    NavigationBar(
-        containerColor = MaterialTheme.colorScheme.background,
-        contentColor = MaterialTheme.colorScheme.background,
-        tonalElevation = 8.dp
-    ) {
-
-        val items = listOf(
-            MainNavigation.Home,
-            MainNavigation.Wishlist,
-            MainNavigation.Cart,
-            MainNavigation.Profile,
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(10.dp),
+        shape = RoundedCornerShape(
+            topStart = 16.dp,
+            topEnd = 16.dp
         )
-        items.forEach {
-            NavigationBarItem(label = { Text(text = it.title) }, colors = DefaultNavigationBarItemTheme(),
-                selected = it.route == currentRoute(navigator),
-                icon = {
-                    Icon(
-                        if (it.route == currentRoute(navigator)) it.selectedIcon else it.unSelectedIcon,
-                        it.title
-                    )
-                },
-                onClick = {
-                    navigator.navigate(
-                        it.route,
-                        NavOptions(
-                            launchSingleTop = true,
-                        ),
-                    )
-                })
+    ) {
+        NavigationBar(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.background,
+            tonalElevation = 8.dp
+        ) {
+
+            val items = listOf(
+                MainNavigation.Home,
+                MainNavigation.Wishlist,
+                MainNavigation.Cart,
+                MainNavigation.Profile,
+            )
+            items.forEach {
+                NavigationBarItem(label = { Text(text = it.title) },
+                    colors = DefaultNavigationBarItemTheme(),
+                    selected = it.route == currentRoute(navigator),
+                    icon = {
+                        Icon(
+                            if (it.route == currentRoute(navigator)) it.selectedIcon else it.unSelectedIcon,
+                            it.title
+                        )
+                    },
+                    onClick = {
+                        navigator.navigate(
+                            it.route,
+                            NavOptions(
+                                launchSingleTop = true,
+                            ),
+                        )
+                    })
+            }
         }
     }
 }
