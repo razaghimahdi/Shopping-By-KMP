8732a3fc2f5e565fc0a2cb8cb6ab2f50437cfa42
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-18 17:33:06 +0330

    - Updated main.android.kt

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,4 +1,12 @@
+import android.app.Application
 import androidx.compose.runtime.Composable
+import common.Context
 import presentation.App
 
-@Composable fun MainView() = App()
+@Composable
+fun MainView(application: Application) {
+
+    App(application)
+}
+
+
