8e16e6600402363c16a5cb7ecf68df34406df052
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-11-27 11:11:11 +0330

    - Updated WishlistScreen

--- shared/src/commonMain/kotlin/presentation/ui/main/wishlist/WishlistScreen.kt
+++ shared/src/commonMain/kotlin/presentation/ui/main/wishlist/WishlistScreen.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -19,6 +23,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import business.constants.PAGINATION_PAGE_SIZE
+import business.core.ProgressBarState
 import business.domain.main.Category
 import business.domain.main.product_sample
 import presentation.component.DefaultScreenUI
@@ -58,10 +64,38 @@ fun WishlistScreen(
             Spacer_8dp()
 
 
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                contentPadding = PaddingValues(8.dp),
+                content = {
+                    itemsIndexed(state.wishlist.products){index, product ->
+
+                        if ((index + 1) >= (state.page * PAGINATION_PAGE_SIZE) &&
+                            state.progressBarState == ProgressBarState.Idle &&
+                            state.hasNextPage
+                        ) {
+                            events(WishlistEvent.GetNextPage)
+                        }
+
+                        ProductBox(
+                            product = product,
+                            modifier = Modifier.fillMaxWidth(.5f),
+                            onLikeClick = {
+                                events(WishlistEvent.LikeProduct(product.id))
+                            }
+                        ) {
+                            navigateToDetail(product.id)
+                        }
+                    }
+                }
+            )
+/*
+
             FlowRow(modifier = Modifier.fillMaxWidth().padding(8.dp), maxItemsInEachRow = 2) {
                 state.wishlist.products.forEach {
                     ProductBox(
-                        product = product_sample,
+                        product = it,
                         modifier = Modifier.fillMaxWidth(.5f),
                         onLikeClick = {
                             events(WishlistEvent.LikeProduct(it.id))
@@ -71,6 +105,7 @@ fun WishlistScreen(
                 }
             }
 
+*/
 
         }
     }
