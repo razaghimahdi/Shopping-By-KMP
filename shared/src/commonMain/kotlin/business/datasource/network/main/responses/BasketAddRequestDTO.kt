1cf1ec012eb83259d40434ab3e47414a2ff079f5
mahdirazzaghi <razaghimahdi78@gmail.com>
2023-12-03 10:02:12 +0330

    - Added BasketAddRequestDTO.kt, BasketDeleteRequestDTO.kt

---
+++ shared/src/commonMain/kotlin/business/datasource/network/main/responses/BasketAddRequestDTO.kt
@@ -0,0 +1,11 @@
+package business.datasource.network.main.responses
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class BasketAddRequestDTO(
+    @SerialName("count") val count: Int,
+    @SerialName("product") val product: Int,
+)
---
+++ shared/src/commonMain/kotlin/business/datasource/network/main/responses/BasketDeleteRequestDTO.kt
@@ -0,0 +1,10 @@
+package business.datasource.network.main.responses
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class BasketDeleteRequestDTO(
+    @SerialName("product") val product: Int,
+)
