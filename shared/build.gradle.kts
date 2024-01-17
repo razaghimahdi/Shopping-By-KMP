8b87a8369ff355196637dacf997e859433618881
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-17 13:10:10 +0330

    - Updated gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -105,6 +105,7 @@ android {
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+    sourceSets["main"].res.srcDirs("src/commonMain/resources", "src/androidMain/res")
 
     defaultConfig {
         minSdk = (findProperty("android.minSdk") as String).toInt()
