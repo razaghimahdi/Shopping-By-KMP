fc2116233e9300d53f7ce0c0df60ba409438df2a
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-28 09:27:47 +0330

    - Added Address.kt

---
+++ shared/src/commonMain/kotlin/business/domain/main/Address.kt
@@ -0,0 +1,13 @@
+package business.domain.main
+
+data class Address(
+    val id: Int = 0,
+    val address: String = "",
+    val country: String = "",
+    val city: String = "",
+    val state: String = "",
+    val zipCode: String = ""
+){
+    fun getFullAddress() = "$state, $city, $address"
+    fun getLocation() = if(city.isEmpty()) "No Location!" else "$city, $country"
+}
--- shared/src/commonMain/kotlin/business/domain/main/Home.kt
+++ shared/src/commonMain/kotlin/business/domain/main/Home.kt
@@ -1,6 +1,7 @@
 package business.domain.main
 
 data class Home(
+    val address: Address = Address(),
     val banners: List<Banner> = listOf(),
     val categories: List<Category> = listOf(),
     val flashSale: FlashSale = FlashSale(),
