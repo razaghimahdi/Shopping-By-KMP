80ddb10363e7b09b74800346c9c166cd7772760d
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-28 15:21:57 +0330

    - Added LogoutInteractor.kt

---
+++ shared/src/commonMain/kotlin/business/interactors/main/LogoutInteractor.kt
@@ -0,0 +1,47 @@
+package business.interactors.main
+
+
+import business.constants.AUTHORIZATION_BEARER_TOKEN
+import business.constants.DataStoreKeys
+import business.core.AppDataStore
+import business.core.DataState
+import business.core.ProgressBarState
+import business.core.UIComponent
+import business.util.handleUseCaseException
+import business.datasource.network.splash.SplashService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class LogoutInteractor(
+    private val appDataStoreManager: AppDataStore,
+) {
+
+
+    fun execute(): Flow<DataState<Boolean>> = flow {
+
+        try {
+
+            emit(DataState.Loading(progressBarState = ProgressBarState.ButtonLoading))
+
+            appDataStoreManager.setValue(
+                DataStoreKeys.TOKEN,
+                ""
+            )
+
+
+
+            emit(DataState.Data(true))
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(handleUseCaseException(e))
+
+        } finally {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Idle))
+        }
+
+
+    }
+
+
+}
