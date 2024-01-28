2ae5701eb6d13f58e2a46d9a55c78ace1a010a5a
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-28 15:20:46 +0330

    - Updated Constants

--- shared/src/commonMain/kotlin/business/constants/Constants.kt
+++ shared/src/commonMain/kotlin/business/constants/Constants.kt
@@ -6,3 +6,5 @@ const val REFRESH_SCREEN = "refresh "
 const val NO_NEED_TO_RESEND = "NO_NEED_TO_RESEND"
 
 const val PAGINATION_PAGE_SIZE = 20
+
+const val LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus eget purus vel pulvinar. Mauris felis odio, posuere sodales rhoncus quis, tempor eget dui. Nam laoreet egestas aliquam. Fusce rhoncus quam non nisl sodales, vel porttitor purus facilisis. Duis tempus leo et tortor ornare eleifend. Sed condimentum lobortis eleifend. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Interdum et malesuada fames ac ante ipsum primis in faucibus."
