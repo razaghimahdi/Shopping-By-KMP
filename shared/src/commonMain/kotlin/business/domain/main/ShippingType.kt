8feebf7b4ff2772724fb872dd821bf98587e3574
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-29 11:48:20 +0330

    - Updated ShippingType.kt

--- shared/src/commonMain/kotlin/business/domain/main/ShippingType.kt
+++ shared/src/commonMain/kotlin/business/domain/main/ShippingType.kt
@@ -1,3 +1,5 @@
 package business.domain.main
 
-data class ShippingType(val title: String, val price: Int, val arrivalDay: Int)
+data class ShippingType(val title: String, val price: Int, val arrivalDay: Int){
+    fun getEstimatedDay() = "Estimated Arrival in $arrivalDay days"
+}
