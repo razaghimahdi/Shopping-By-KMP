90b16af18993d23263ee22bc69efef45bf9b1acd
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-29 11:48:06 +0330

    - Updated ComposeFileProvider

--- shared/src/androidMain/kotlin/ComposeFileProvider.kt
+++ shared/src/androidMain/kotlin/ComposeFileProvider.kt
@@ -10,16 +10,12 @@ class ComposeFileProvider : FileProvider(
 ) {
     companion object {
         fun getImageUri(context: Context): Uri {
-            // 1
             val tempFile = File.createTempFile(
                 "picture_${System.currentTimeMillis()}", ".png", context.cacheDir
             ).apply {
                 createNewFile()
             }
-            // 2
             val authority = context.applicationContext.packageName + ".provider"
-            // 3
-            println("getImageUri: ${tempFile.absolutePath}")
             return getUriForFile(
                 Objects.requireNonNull(context),
                 authority,
--- shared/src/androidMain/kotlin/common/CameraManager.android.kt
+++ shared/src/androidMain/kotlin/common/CameraManager.android.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import ComposeFileProvider
 
 @Composable
 actual fun rememberCameraManager(onResult: (SharedImage?) -> Unit): CameraManager {
