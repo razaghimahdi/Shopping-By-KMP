2fed36054256ff34ababc4407d5728e0dad9d7c1
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-01-30 19:42:36 +0330

    - Updated presentation

--- shared/src/commonMain/kotlin/presentation/component/SortDialog.kt
+++ shared/src/commonMain/kotlin/presentation/component/SortDialog.kt
@@ -1,33 +1,26 @@
 package presentation.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RangeSlider
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import business.constants.Sort.HIGHER_PRICE
+import business.constants.Sort.LOWEST_PRICE
+import business.constants.Sort.MOST_SALE
+import business.constants.Sort.NEWEST
+import business.constants.Sort.OLDEST
 import business.core.UIComponentState
-import business.domain.main.Category
-import business.domain.main.Search
-import business.domain.main.SearchFilter
 import presentation.ui.main.search.view_model.SearchEvent
 import presentation.ui.main.search.view_model.SearchState
 
@@ -64,42 +57,42 @@ fun SortDialog(
             Spacer_16dp()
 
             Row(modifier = Modifier.fillMaxWidth().noRippleClickable {
-                events(SearchEvent.OnUpdateSelectedSort(0))
+                events(SearchEvent.OnUpdateSelectedSort(NEWEST))
                 events(SearchEvent.OnUpdateSortDialogState(UIComponentState.Hide))
             }, verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = selectedSort.value == 0, onCheckedChange = {})
+                Checkbox(checked = selectedSort.value == NEWEST, onCheckedChange = {})
                 Text("Newest", style = MaterialTheme.typography.labelLarge)
             }
 
             Row(modifier = Modifier.fillMaxWidth().noRippleClickable {
-                events(SearchEvent.OnUpdateSelectedSort(1))
+                events(SearchEvent.OnUpdateSelectedSort(OLDEST))
                 events(SearchEvent.OnUpdateSortDialogState(UIComponentState.Hide))
             }, verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = selectedSort.value == 1, onCheckedChange = {})
+                Checkbox(checked = selectedSort.value == OLDEST, onCheckedChange = {})
                 Text("Oldest", style = MaterialTheme.typography.labelLarge)
             }
 
             Row(modifier = Modifier.fillMaxWidth().noRippleClickable {
-                events(SearchEvent.OnUpdateSelectedSort(2))
+                events(SearchEvent.OnUpdateSelectedSort(HIGHER_PRICE))
                 events(SearchEvent.OnUpdateSortDialogState(UIComponentState.Hide))
             }, verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = selectedSort.value == 2, onCheckedChange = {})
+                Checkbox(checked = selectedSort.value == HIGHER_PRICE, onCheckedChange = {})
                 Text("Highest Price", style = MaterialTheme.typography.labelLarge)
             }
 
             Row(modifier = Modifier.fillMaxWidth().noRippleClickable {
-                events(SearchEvent.OnUpdateSelectedSort(3))
+                events(SearchEvent.OnUpdateSelectedSort(LOWEST_PRICE))
                 events(SearchEvent.OnUpdateSortDialogState(UIComponentState.Hide))
             }, verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = selectedSort.value == 3, onCheckedChange = {})
+                Checkbox(checked = selectedSort.value == LOWEST_PRICE, onCheckedChange = {})
                 Text("Lowest Price", style = MaterialTheme.typography.labelLarge)
             }
 
             Row(modifier = Modifier.fillMaxWidth().noRippleClickable {
-                events(SearchEvent.OnUpdateSelectedSort(4))
+                events(SearchEvent.OnUpdateSelectedSort(MOST_SALE))
                 events(SearchEvent.OnUpdateSortDialogState(UIComponentState.Hide))
             }, verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = selectedSort.value == 4, onCheckedChange = {})
+                Checkbox(checked = selectedSort.value == MOST_SALE, onCheckedChange = {})
                 Text("Most Sale", style = MaterialTheme.typography.labelLarge)
             }
 
