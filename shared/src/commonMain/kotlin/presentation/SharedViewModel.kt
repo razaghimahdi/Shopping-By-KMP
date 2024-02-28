102d162f34be7043b23eb8d4f0fbd2fe772876a8
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-02-28 18:39:20 +0330

    - Added SharedViewModel.kt

---
+++ shared/src/commonMain/kotlin/presentation/SharedViewModel.kt
@@ -0,0 +1,9 @@
+package presentation
+
+import moe.tlaster.precompose.viewmodel.ViewModel
+import presentation.token_manager.TokenManager
+
+class SharedViewModel(
+    val tokenManager: TokenManager,
+) : ViewModel() {
+}
