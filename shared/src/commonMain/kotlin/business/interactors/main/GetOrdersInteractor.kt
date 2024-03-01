ebbdd3b07bee83e49a1b4cffcca653b388b885dc
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-03-01 16:09:05 +0330

    - Added GetOrdersInteractor.kt

---
+++ shared/src/commonMain/kotlin/business/interactors/main/GetOrdersInteractor.kt
@@ -0,0 +1,62 @@
+package business.interactors.main
+
+
+import business.constants.DataStoreKeys
+import business.core.AppDataStore
+import business.core.DataState
+import business.core.NetworkState
+import business.core.ProgressBarState
+import business.datasource.network.main.MainService
+import business.datasource.network.main.responses.toOrder
+import business.domain.main.Order
+import business.util.createException
+import business.util.handleUseCaseException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetOrdersInteractor(
+    private val service: MainService,
+    private val appDataStoreManager: AppDataStore,
+) {
+
+
+    fun execute(): Flow<DataState<List<Order>>> = flow {
+
+        try {
+
+            emit(DataState.Loading(progressBarState = ProgressBarState.LoadingWithLogo))
+
+            val token = appDataStoreManager.readValue(DataStoreKeys.TOKEN) ?: ""
+
+
+            val apiResponse = service.getOrders(token = token)
+
+
+
+            if (apiResponse.status == false) {
+                throw Exception(
+                    apiResponse.alert?.createException()
+                )
+            }
+
+
+            val result = apiResponse.result?.map { it.toOrder() }
+
+
+            emit(DataState.NetworkStatus(NetworkState.Good))
+            emit(DataState.Data(result))
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(DataState.NetworkStatus(NetworkState.Failed))
+            emit(handleUseCaseException(e))
+
+        } finally {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Idle))
+        }
+
+
+    }
+
+
+}
