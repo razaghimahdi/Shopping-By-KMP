31d42474f44b6fdf00197edc5fd8ab23dd4509e7
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-03-01 16:08:43 +0330

    - Updated Order.kt

--- shared/src/commonMain/kotlin/business/domain/main/Order.kt
+++ shared/src/commonMain/kotlin/business/domain/main/Order.kt
@@ -1,12 +1,12 @@
 package business.domain.main
 
 data class Order(
-    val id: Int,
     val products: List<Product>,
     val status: Int,
     val code: String,
-    val createAt: String,
-    val amount: String,
+    val createdAt: String,
     val address: Address,
     val shippingType: ShippingType,
-)
+) {
+    fun getAmount() = "$ ${products.sumOf { it.price } + shippingType.price}"
+}
