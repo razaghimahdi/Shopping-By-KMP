4fe5ee77919d471c9d45365cb4f67e5127003985
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-03-07 12:22:01 +0330

    - Added ExampleTest.kt: lets write some UI test :)

---
+++ shared/src/commonTest/kotlin/presentation/ExampleTest.kt
@@ -0,0 +1,45 @@
+package presentation
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
+class ExampleTest {
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun myTest() = runComposeUiTest {
+        // Declares a mock UI to demonstrate API calls
+        //
+        // Replace with your own declarations to test the code of your project
+        setContent {
+            var text by remember { mutableStateOf("Hello") }
+            Text(
+                text = text,
+                modifier = Modifier.testTag("text")
+            )
+            Button(
+                onClick = { text = "Compose" },
+                modifier = Modifier.testTag("button")
+            ) {
+                Text("Click me")
+            }
+        }
+
+        // Tests the declared UI with assertions and actions of the Compose Multiplatform testing API
+        onNodeWithTag("text").assertTextEquals("Hello")
+        onNodeWithTag("button").performClick()
+        onNodeWithTag("text").assertTextEquals("Compose")
+    }
+}
