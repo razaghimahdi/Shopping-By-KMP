e178071eb06cc790d053abc56dd4f77056ae0c84
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-03-08 08:09:08 +0330

    - Added HomeScreenTest.kt

---
+++ shared/src/commonTest/kotlin/presentation/ui/main/home/HomeScreenTest.kt
@@ -0,0 +1,38 @@
+package presentation.ui.main.home
+
+import androidx.compose.runtime.remember
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import business.datasoruce.network.main.HomeFakeDataGenerator
+import business.datasoruce.serializeHomeData
+import presentation.ui.main.home.view_model.HomeState
+import kotlin.test.Test
+
+class HomeScreenTest {
+
+
+    private val homeData = serializeHomeData(HomeFakeDataGenerator.goodData)
+
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun `image slider and categories and flash sale and most sale and newest products and selected location are shown`() =
+        runComposeUiTest {
+            setContent {
+                val state = remember {
+                    HomeState(home = homeData)
+                }
+                HomeScreen(state = state)
+            }
+            //onRoot().printToLog("UI Hierarchy")
+
+
+            onNodeWithText("Computer").assertIsDisplayed()
+            onAllNodesWithText("Nike model-934")[0].assertIsDisplayed()
+        }
+
+
+}
