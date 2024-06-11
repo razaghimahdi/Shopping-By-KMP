4b7c198b83849d11ad962713ca104213e4ecb891
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-06-11 22:38:49 +0330

    - Updated SharedImage: compress image after capture or selecting image

--- shared/src/androidMain/kotlin/common/SharedImage.android.kt
+++ shared/src/androidMain/kotlin/common/SharedImage.android.kt
@@ -3,18 +3,19 @@ package common
 import android.graphics.BitmapFactory
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 
 actual class SharedImage(private val bitmap: android.graphics.Bitmap?) {
     actual fun toByteArray(): ByteArray? {
+       // val compressedBitmap = compressImage(bitmap ?: return null)
         return if (bitmap != null) {
             val byteArrayOutputStream = ByteArrayOutputStream()
-            @Suppress("MagicNumber") bitmap.compress(
+            bitmap.compress(
                 android.graphics.Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream
             )
             byteArrayOutputStream.toByteArray()
         } else {
-            println("toByteArray null")
             null
         }
     }
@@ -22,10 +23,33 @@ actual class SharedImage(private val bitmap: android.graphics.Bitmap?) {
     actual fun toImageBitmap(): ImageBitmap? {
         val byteArray = toByteArray()
         return if (byteArray != null) {
-            return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size).asImageBitmap()
+            return compressImage(BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size))?.asImageBitmap()
         } else {
-            println("toImageBitmap null")
             null
         }
     }
+}
+
+
+private fun compressImage(bmp: android.graphics.Bitmap): android.graphics.Bitmap? {
+    val baos = ByteArrayOutputStream()
+    bmp.compress(
+        android.graphics.Bitmap.CompressFormat.PNG,
+        100,
+        baos
+    )
+    var options = 90
+    while (baos.toByteArray().size / 1024 > 400) {
+        baos.reset()
+        bmp.compress(
+            android.graphics.Bitmap.CompressFormat.PNG,
+            options,
+            baos
+        )
+        options -= 10
+    }
+    val isBm =
+        ByteArrayInputStream(baos.toByteArray())
+
+    return BitmapFactory.decodeStream(isBm, null, null)
 }
--- shared/src/iosMain/kotlin/common/SharedImage.ios.kt
+++ shared/src/iosMain/kotlin/common/SharedImage.ios.kt
@@ -14,8 +14,9 @@ import platform.UIKit.UIImageJPEGRepresentation
 actual class SharedImage(private val image: UIImage?) {
     @OptIn(ExperimentalForeignApi::class)
     actual fun toByteArray(): ByteArray? {
-        return if (image != null) {
-            val imageData = UIImageJPEGRepresentation(image, COMPRESSION_QUALITY)
+        val compressedImage = compressImage(image ?: return null)
+        return if (compressedImage != null) {
+            val imageData = UIImageJPEGRepresentation(compressedImage, COMPRESSION_QUALITY)
                 ?: throw IllegalArgumentException("image data is null")
             val bytes = imageData.bytes ?: throw IllegalArgumentException("image bytes is null")
             val length = imageData.length
@@ -28,6 +29,7 @@ actual class SharedImage(private val image: UIImage?) {
 
     }
 
+
     actual fun toImageBitmap(): ImageBitmap? {
         val byteArray = toByteArray()
         return if (byteArray != null) {
@@ -39,5 +41,33 @@ actual class SharedImage(private val image: UIImage?) {
 
     private companion object {
         const val COMPRESSION_QUALITY = 0.99
+    }
+
+}
+
+
+private fun compressImage(uiImage: UIImage): UIImage? {
+    var photo = uiImage
+    var resizedImageData = UIImageJPEGRepresentation(photo, 1.0)
+    var imageSize: Int = resizedImageData?.length?.toInt() ?: -1
+
+    if (imageSize == -1) return null
+    if (resizedImageData == null) return null
+
+    var tryCounter: Int = 5
+    while (imageSize > 500000) {
+        resizedImageData = UIImageJPEGRepresentation(photo, 0.75)
+        imageSize = resizedImageData?.length?.toInt() ?: -1
+        if (resizedImageData == null) return null
+        photo = UIImage(data = resizedImageData, scale = 0.0)
+        tryCounter -= 1
+        if (tryCounter == 0) {
+            return null
+        }
+    }
+    if (imageSize < 500000) {
+        return photo
+    } else {
+        return null
     }
 }
