3a2b1ab3253131f30851ebafc9efa0695a95079f
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-07-17 22:02:14 +0330

    - Updated gradle - Updated libs.versions.toml

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,3 +2,47 @@ plugins {
     alias(libs.plugins.shopping.kotlinMultiplatform)
     alias(libs.plugins.shopping.shared)
 }
+
+
+kotlin {
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+
+        commonTest {
+            dependencies {
+
+            }
+        }
+        commonMain {
+            dependencies {
+
+            }
+        }
+
+        androidMain {
+            dependencies {
+
+
+            }
+        }
+        iosMain {
+            dependencies {
+                
+            }
+        }
+
+    }
+}
+
+
