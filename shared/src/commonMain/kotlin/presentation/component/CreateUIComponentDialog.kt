e97102486c2a95ed981a01a4dd8980f4e867d8dd
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-01-06 12:15:04 +0330

    - Updated DefaultScreenUI: refactor error handling

--- shared/src/commonMain/kotlin/presentation/component/CreateUIComponentDialog.kt
+++ shared/src/commonMain/kotlin/presentation/component/CreateUIComponentDialog.kt
@@ -3,9 +3,7 @@ package presentation.component
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- shared/src/commonMain/kotlin/presentation/component/DefaultScreenUI.kt
+++ shared/src/commonMain/kotlin/presentation/component/DefaultScreenUI.kt
@@ -17,28 +17,34 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import business.constants.CUSTOM_TAG
 import business.core.NetworkState
 import business.core.ProgressBarState
 import business.core.Queue
 import business.core.UIComponent
+import business.core.ViewSingleAction
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import shoping_by_kmp.shared.generated.resources.Res
 import shoping_by_kmp.shared.generated.resources.no_wifi
 
-/**
- * @param queue: Dialogs
- * @param content: The content of the UI.
- */
+
 @Composable
 fun DefaultScreenUI(
-    queue: Queue<UIComponent> = Queue(mutableListOf()),
-    onRemoveHeadFromQueue: () -> Unit = {},
+    errors: Flow<UIComponent> = MutableSharedFlow(),
     progressBarState: ProgressBarState = ProgressBarState.Idle,
     networkState: NetworkState = NetworkState.Good,
     onTryAgain: () -> Unit = {},
@@ -49,6 +55,11 @@ fun DefaultScreenUI(
     onClickEndIconToolbar: () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
+
+    val errorQueue = remember {
+        mutableStateOf<Queue<UIComponent>>(Queue(mutableListOf()))
+    }
+
 
     Scaffold(
         topBar = {
@@ -85,26 +96,36 @@ fun DefaultScreenUI(
             contentAlignment = Alignment.Center
         ) {
             content()
+
+
+            LaunchedEffect(errors) {
+                errors.collect { errors ->
+                    errorQueue.appendToMessageQueue(errors)
+                }
+            }
+
+
             // process the queue
-            if (!queue.isEmpty()) {
-                queue.peek()?.let { uiComponent ->
+            if (!errorQueue.value.isEmpty()) {
+                errorQueue.value.peek()?.let { uiComponent ->
                     if (uiComponent is UIComponent.Dialog) {
                         CreateUIComponentDialog(
                             title = uiComponent.alert.title,
                             description = uiComponent.alert.message,
-                            onRemoveHeadFromQueue = onRemoveHeadFromQueue
+                            onRemoveHeadFromQueue = { errorQueue.removeHeadMessage() }
                         )
                     }
                     if (uiComponent is UIComponent.ToastSimple) {
                         ShowSnackbar(
                             title = uiComponent.title,
                             snackbarVisibleState = true,
-                            onDismiss = onRemoveHeadFromQueue,
+                            onDismiss = { errorQueue.removeHeadMessage() },
                             modifier = Modifier.align(Alignment.BottomCenter)
                         )
                     }
                 }
             }
+
             if (networkState == NetworkState.Failed && progressBarState == ProgressBarState.Idle) {
                 FailedNetworkScreen(onTryAgain = onTryAgain)
             }
@@ -124,7 +145,44 @@ fun DefaultScreenUI(
     }
 }
 
-@OptIn(ExperimentalResourceApi::class)
+
+private fun MutableState<Queue<UIComponent>>.appendToMessageQueue(uiComponent: UIComponent) {
+    if (uiComponent is UIComponent.None) {
+        println("$CUSTOM_TAG appendToMessageQueue:  ${uiComponent.message}")
+        return
+    }
+
+    val queue = this.value
+    queue.add(uiComponent)
+
+    this.value = Queue(mutableListOf()) // force to recompose
+    this.value = queue
+}
+
+private fun MutableState<Queue<UIComponent>>.removeHeadMessage() {
+    if (this.value.isEmpty()) {
+        println("$CUSTOM_TAG: removeHeadMessage: Nothing to remove from DialogQueue")
+        return
+    }
+    val queue = this.value
+    queue.remove() // can throw exception if empty
+    this.value = Queue(mutableListOf()) // force to recompose
+    this.value = queue
+}
+
+
+@Composable
+fun <Effect : ViewSingleAction> EffectHandler(
+    effectFlow: Flow<Effect>,
+    onHandleEffect: (Effect) -> Unit
+) {
+    LaunchedEffect(Unit) {
+        effectFlow.collect { effect ->
+            onHandleEffect(effect)
+        }
+    }
+}
+
 @Composable
 
 fun FailedNetworkScreen(onTryAgain: () -> Unit) {
