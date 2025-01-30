28ebd0f1434ef116a4aa50a65d3b019017a6a2b4
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-01-30 18:42:29 +0330

    - Updated TokenManager

--- shared/src/commonMain/kotlin/presentation/token_manager/TokenManager.kt
+++ shared/src/commonMain/kotlin/presentation/token_manager/TokenManager.kt
@@ -3,16 +3,17 @@ package presentation.token_manager
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import business.core.DataState
-import business.interactors.main.LogoutInteractor
-import business.interactors.splash.CheckTokenInteractor
+import business.interactors.main.AddAddressUseCase
+import business.interactors.main.LogoutUseCase
+import business.interactors.splash.CheckTokenUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 class TokenManager(
-    private val checkTokenInteractor: CheckTokenInteractor,
-    private val logoutInteractor: LogoutInteractor,
+    private val checkTokenUseCase: CheckTokenUseCase,
+    private val logoutUseCase: LogoutUseCase,
 ) {
 
     private val sessionScope = CoroutineScope(Dispatchers.Main)
@@ -35,7 +36,7 @@ class TokenManager(
     }
 
     private fun checkToken() {
-        checkTokenInteractor.execute().onEach { dataState ->
+        checkTokenUseCase.execute(Unit).onEach { dataState ->
             when (dataState) {
                 is DataState.NetworkStatus -> {}
                 is DataState.Response -> {}
@@ -49,7 +50,7 @@ class TokenManager(
     }
 
     private fun logout() {
-        logoutInteractor.execute().onEach { dataState ->
+        logoutUseCase.execute(Unit).onEach { dataState ->
             when (dataState) {
                 is DataState.NetworkStatus -> {}
                 is DataState.Response -> {}
