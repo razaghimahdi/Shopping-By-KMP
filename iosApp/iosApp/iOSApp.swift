5de21c233bd4ecf89f9bfd13dec4abfd92ed29ff
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-02-21 22:10:49 +0330

    - Updated iosApp: Added LocationManager.swift, updated ContentView.swift to manage and show map in swift

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 import shared
+import GoogleMaps
+
 
 @main
 struct iOSApp: App {
+    init() {
+        GMSServices.provideAPIKey("YOUR_API_KEY")
+    }
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
