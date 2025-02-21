a5d71f8a8ed98904018f29c620fe6639c29515fb
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-02-21 21:58:31 +0330

    - Updated main.ios.kt

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,6 +1,24 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import common.Context
+import platform.UIKit.UIViewController
 import presentation.App
 
-fun mainViewController() = ComposeUIViewController { App(Context()) }
+
+fun mainViewController(
+    mapUIViewController: () -> UIViewController,
+    latitude: Double,
+    longitude: Double,
+) = ComposeUIViewController {
+    mapViewController = mapUIViewController
+    globalLatitude = latitude
+    globalLongitude = longitude
+    App(Context())
+}
+
+lateinit var mapViewController: () -> UIViewController
+var globalLatitude: Double = 0.0
+var globalLongitude: Double = 0.0
+
+
+// fun mainViewController() = ComposeUIViewController { App(Context()) }
 
