aa73f9fee754c6cefce4422828e6f99acf062d5f
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-03-31 17:30:49 +0330

    - Updated SplashScreen: refactored ui

--- shared/src/commonMain/kotlin/presentation/ui/splash/SplashScreen.kt
+++ shared/src/commonMain/kotlin/presentation/ui/splash/SplashScreen.kt
@@ -10,12 +10,16 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -27,25 +31,34 @@ import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import presentation.component.Spacer_32dp
 import shoping_by_kmp.shared.generated.resources.Res
 import shoping_by_kmp.shared.generated.resources.img_headphone_splash
 import shoping_by_kmp.shared.generated.resources.img_shoe1_splash
 import shoping_by_kmp.shared.generated.resources.img_shoe2_splash
 import shoping_by_kmp.shared.generated.resources.img_watch_splash
+import shoping_by_kmp.shared.generated.resources.splash_title2
+import shoping_by_kmp.shared.generated.resources.stallion_beatsides_regular
 
-fun getSplashList() = listOf(
+private fun getSplashList() = listOf(
     Res.drawable.img_headphone_splash,
     Res.drawable.img_shoe2_splash,
     Res.drawable.img_watch_splash,
@@ -55,7 +68,7 @@ fun getSplashList() = listOf(
 @Composable
 internal fun SplashScreen() {
 
-    val pagerState = rememberPagerState(initialPage = 0, pageCount = { getSplashList().size })
+    val pagerState = rememberPagerState(initialPage = 1, pageCount = { getSplashList().size })
     val showProducts = produceState(initialValue = false) {
         delay(1000)
         value = true
@@ -66,63 +79,83 @@ internal fun SplashScreen() {
     ) {
         Circle(modifier = Modifier.align(Alignment.TopCenter))
 
-        AnimatedVisibility(
-            showProducts.value,
-            enter = slideInHorizontally(
-                initialOffsetX = { it },
-                animationSpec = tween(durationMillis = 500)
-            ),
-            exit = slideOutHorizontally(
-                targetOffsetX = { -it },
-                animationSpec = tween(durationMillis = 500)
-            )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
         ) {
 
-            HorizontalPager(
-                state = pagerState,
-                contentPadding = PaddingValues(end = 50.dp, start = 140.dp),
-                userScrollEnabled = true,
-                flingBehavior = PagerDefaults.flingBehavior(
-                    state = pagerState,
-                    snapPositionalThreshold = 0.25f
-                ),
-                modifier = Modifier
-                    .fillMaxSize()
-                    .drawWithCache {
-                        onDrawWithContent {
-                            drawContent()
-                        }
-                    },
-                verticalAlignment = Alignment.CenterVertically,
-            ) { page ->
-
-                val isCurrent = page == pagerState.currentPage
-
-                val widthAnim = animateDpAsState(targetValue = if (isCurrent) 150.dp else 120.dp)
-                val heightAnim = animateDpAsState(targetValue = if (isCurrent) 200.dp else 150.dp)
-
-
-                val infiniteTransition = rememberInfiniteTransition(label = "floating")
-
-                val offsetY by infiniteTransition.animateFloat(
-                    initialValue = 0f,
-                    targetValue = if (isCurrent) 20f else 0f,
-                    animationSpec = infiniteRepeatable(
-                        animation = tween(durationMillis = 2000, easing = LinearEasing),
-                        repeatMode = RepeatMode.Reverse
-                    ), label = "offsetY"
-                )
-
-                Image(
-                    painter = painterResource(getSplashList()[page]),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillBounds,
-                    modifier = Modifier.width(widthAnim.value).height(heightAnim.value)
-                        .offset(y = offsetY.dp)
+            AnimatedVisibility(
+                showProducts.value,
+                enter = fadeIn(animationSpec = tween(500)),
+                exit = fadeOut(animationSpec = tween(500))
+            ) {
+                Text(
+                    stringResource(Res.string.splash_title2), fontFamily = FontFamily(
+                        Font(
+                            resource = Res.font.stallion_beatsides_regular,
+                            weight = FontWeight.Normal,
+                            style = FontStyle.Normal
+                        )
+                    ),
+                    color = MaterialTheme.colorScheme.background,
+                    fontSize = 58.sp
                 )
             }
-        }
 
+            Spacer_32dp()
+
+            AnimatedVisibility(
+                showProducts.value,
+                enter = slideInHorizontally(
+                    initialOffsetX = { it },
+                    animationSpec = tween(durationMillis = 500)
+                ),
+                exit = slideOutHorizontally(
+                    targetOffsetX = { -it },
+                    animationSpec = tween(durationMillis = 500)
+                )
+            ) {
+
+                HorizontalPager(
+                    state = pagerState,
+                    contentPadding = PaddingValues(end = 50.dp, start = 140.dp),
+                    userScrollEnabled = false,
+                    flingBehavior = PagerDefaults.flingBehavior(
+                        state = pagerState,
+                        snapPositionalThreshold = 0.25f
+                    ),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) { page ->
+
+                    val isCurrent = page == pagerState.currentPage
+
+                    val widthAnim =
+                        animateDpAsState(targetValue = if (isCurrent) 150.dp else 120.dp)
+                    val heightAnim =
+                        animateDpAsState(targetValue = if (isCurrent) 200.dp else 150.dp)
+
+
+                    val infiniteTransition = rememberInfiniteTransition(label = "floating")
+
+                    val offsetY by infiniteTransition.animateFloat(
+                        initialValue = 0f,
+                        targetValue = if (isCurrent) 20f else 0f,
+                        animationSpec = infiniteRepeatable(
+                            animation = tween(durationMillis = 2000, easing = LinearEasing),
+                            repeatMode = RepeatMode.Reverse
+                        ), label = "offsetY"
+                    )
+
+                    Image(
+                        painter = painterResource(getSplashList()[page]),
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.width(widthAnim.value).height(heightAnim.value)
+                            .offset(y = offsetY.dp)
+                    )
+                }
+            }
+        }
         CircularProgressIndicator(modifier = Modifier.align(Alignment.BottomCenter).padding(16.dp))
 
 
