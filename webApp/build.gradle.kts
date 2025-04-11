3c2b24838ac0d6f1b3ddaaea5129b3d5e5460bd4
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-04-11 11:28:59 +0330

    - Updated build.gradle.kts

--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
@@ -11,10 +13,67 @@ kotlin {
         browser {
             commonWebpackConfig {
                 outputFileName = "webApp.js"
+
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.rootDir.path)
+                        add(project.rootDir.path + "/shared/")
+                        add(project.rootDir.path + "/nonAndroidMain/")
+                        add(project.rootDir.path + "/webApp/")
+                    }
+                }
             }
         }
         binaries.executable()
     }
+
+
+
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+    wasmJs {
+        moduleName = "webApp"
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.rootDir.path)
+                        add(project.rootDir.path + "/shared/")
+                        add(project.rootDir.path + "/nonAndroidMain/")
+                        add(project.rootDir.path + "/webApp/")
+                    }
+                }
+            }
+        }
+        binaries.executable()
+    }
+
+    val copyJsResources = tasks.create("copyJsResourcesWorkaround", Copy::class.java) {
+        from(project(":shared").file("src/commonMain/composeResources"))
+        into("build/processedResources/js/main")
+    }
+
+    val copyWasmResources = tasks.create("copyWasmResourcesWorkaround", Copy::class.java) {
+        from(project(":shared").file("src/commonMain/composeResources"))
+        into("build/processedResources/wasmJs/main")
+    }
+
+    afterEvaluate {
+        project.tasks.getByName("jsProcessResources").finalizedBy(copyJsResources)
+        project.tasks.getByName("wasmJsProcessResources").finalizedBy(copyWasmResources)
+//    project.tasks.getByName("jsBrowserProductionExecutableDistributeResources").mustRunAfter(copyJsResources)
+        project.tasks.getByName("jsDevelopmentExecutableCompileSync").mustRunAfter(copyJsResources)
+        project.tasks.getByName("wasmJsDevelopmentExecutableCompileSync").mustRunAfter(copyWasmResources)
+        project.tasks.getByName("jsProductionExecutableCompileSync").mustRunAfter(copyJsResources)
+        project.tasks.getByName("wasmJsProductionExecutableCompileSync").mustRunAfter(copyWasmResources)
+    }
+
+
+
+
 
     sourceSets {
         val jsMain by getting {
