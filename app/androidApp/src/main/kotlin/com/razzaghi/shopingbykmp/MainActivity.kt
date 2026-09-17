3c31abfa5c413aa5adaa97925b194cda2129fd92
m.razaghi <m.razaghi@atlasdtco.com>
2026-09-17 17:09:12 +0330

    Updated MainActivity.kt

--- app/androidApp/src/main/kotlin/com/razzaghi/shopingbykmp/MainActivity.kt
+++ app/androidApp/src/main/kotlin/com/razzaghi/shopingbykmp/MainActivity.kt
@@ -6,11 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.razzaghi.shopingbykmp.presentation.App
+import com.razzaghi.shopingbykmp.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        if (GlobalContext.getOrNull() == null) {
+            startKoin {
+                androidContext(this@MainActivity.applicationContext)
+                modules(appModule())
+            }
+        }
 
         setContent {
             App()
