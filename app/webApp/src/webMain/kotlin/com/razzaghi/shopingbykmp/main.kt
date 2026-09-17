bf3f3d48b02c7afbf06a9e1941aad8d4647b681c
m.razaghi <m.razaghi@atlasdtco.com>
2026-09-17 17:04:41 +0330

    Updated web

--- app/webApp/src/webMain/kotlin/com/razzaghi/shopingbykmp/main.kt
+++ app/webApp/src/webMain/kotlin/com/razzaghi/shopingbykmp/main.kt
@@ -2,10 +2,22 @@ package com.razzaghi.shopingbykmp
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.razzaghi.shopingbykmp.presentation.App
+import com.razzaghi.shopingbykmp.di.appModule
+import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport {
+
+    try {
+        startKoin {
+            modules(appModule())
+        }
+    } catch (e: Exception) {
+        println("Koin is already started.")
+    }
+
+    ComposeViewport(viewportContainerId = "ComposeTarget") {
         App()
     }
 }
