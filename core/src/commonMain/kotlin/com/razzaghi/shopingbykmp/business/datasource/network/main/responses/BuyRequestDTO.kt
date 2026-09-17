63578c768e53d343a3540625a04ad6e46267d2a4
m.razaghi <m.razaghi@atlasdtco.com>
2026-09-17 17:05:25 +0330

    Added new dto

---
+++ core/src/commonMain/kotlin/com/razzaghi/shopingbykmp/business/datasource/network/main/responses/BuyRequestDTO.kt
@@ -0,0 +1,11 @@
+package com.razzaghi.shopingbykmp.business.datasource.network.main.responses
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class BuyRequestDTO(
+    @SerialName("address_id")  val addressId: Long,
+    @SerialName("shipping_type")  val shippingType: Int,
+    )
