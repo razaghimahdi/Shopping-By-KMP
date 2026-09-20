595b8dc186a26c0cfade84e65d93e8fb0e220c3c
m.razaghi <m.razaghi@atlasdtco.com>
2026-09-20 13:48:30 +0330

    Updated business layer

--- app/shared/src/commonMain/kotlin/com/razzaghi/shopingbykmp/business/constants/KtorConstants.kt
+++ app/shared/src/commonMain/kotlin/com/razzaghi/shopingbykmp/business/constants/KtorConstants.kt
@@ -1,3 +1,9 @@
 package com.razzaghi.shopingbykmp.business.constants
 
-const val BASE_URL = "http://localhost:8080/"
+import com.razzaghi.shopingbykmp.getPlatform
+
+val BASE_URL = if (getPlatform().name.lowercase().contains("android")) {
+    "http://10.0.2.2:8080"
+} else {
+    "http://localhost:8080"
+}
