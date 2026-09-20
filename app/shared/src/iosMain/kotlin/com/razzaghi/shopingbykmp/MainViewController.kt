b8cd990c523f1b506ee85f361da8a03e7488e684
m.razaghi <m.razaghi@atlasdtco.com>
2026-09-20 13:45:48 +0330

    Updated MainViewController.kt

--- app/shared/src/iosMain/kotlin/com/razzaghi/shopingbykmp/MainViewController.kt
+++ app/shared/src/iosMain/kotlin/com/razzaghi/shopingbykmp/MainViewController.kt
@@ -2,5 +2,22 @@ package com.razzaghi.shopingbykmp
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.razzaghi.shopingbykmp.presentation.App
+import platform.UIKit.UIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+
+fun mainViewController(
+    mapUIViewController: () -> UIViewController,
+    latitude: Double,
+    longitude: Double,
+) = ComposeUIViewController {
+    mapViewController = mapUIViewController
+    globalLatitude = latitude
+    globalLongitude = longitude
+    App()
+}
+
+lateinit var mapViewController: () -> UIViewController
+var globalLatitude: Double = 0.0
+var globalLongitude: Double = 0.0
+
+
